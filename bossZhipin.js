04260871f6722cce26b98d97c3da8f5dac2ae385
zhangxue <zhangxue@neikongyi.com>
2019-05-13 00:48:40 +0800

    ...

--- bossZhipin.js
+++ bossZhipin.js
@@ -24,7 +24,29 @@ async function run() {
 
   for (let i = 1; i <= 10; i++) {
     let result = await page.$$('.job-primary>.info-primary h3 > a');
-    console.log(result)
+    if(result && result.length > 0){
+      let url = await page.url();
+      let urlObj = new URL(url);
+      let currentPage = ~~(urlObj.searchParams.get('page'));
+      let activePageAElement = await page.$('.page > .cur');
+      let nextPageAElement = await page.$('.page > .cur + a');
+      let activePage = ~~(activePageAElement.text || '');
+      let nextPage = ~~(nextPageAElement.text || '');
+      if(currentPage === activePage) {
+        // url页码和页面中的当前页码相等，说明没有超出页码范围
+
+
+        if(nextPage){
+          // 点击下一页
+          await Promise.all([
+            page.waitForNavigation(),
+            page.click('.page > .cur + a')
+          ]);
+        }
+      } else {
+        break;
+      }
+    }
     let currentPageUrls = Array.from(result || []).map((a) => {
       return a.href;
     });
